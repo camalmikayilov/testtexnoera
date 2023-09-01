41d36ccd2b34a89a2c7cc7c494ce0655b40235a6
camalmikayilov <camalmikayilov1@gmail.com>
2023-09-01 19:41:17 +0400

    create project

---
+++ main.js
@@ -0,0 +1,19 @@
+// First Solv
+// let a = 5;
+// let b = 7;
+
+// [a, b] = [b, a];
+
+// console.log("a:", a); // Output: a: 7
+// console.log("b:", b); // Output: b: 5
+
+// Second solv
+// let a = 5;
+// let b = 7;
+
+// a = a + b;
+// b = a - b;
+// a = a - b;
+
+// console.log("a:", a); // Output: a: 7
+// console.log("b:", b); // Output: b: 5
